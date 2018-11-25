0b137d2a2e4abb6b4db15b5985df835088531a6d
Robin Gupta <robgup@microsoft.com>
2018-11-25 15:50:19 +0530

    fixing lint

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,7 +6,7 @@ class Quiz extends Component {
 
     constructor(props) {
         super(props);
-        this.state= {quiz_position:1};
+        this.state = { quiz_position: 1 };
     }
 
     render() {
